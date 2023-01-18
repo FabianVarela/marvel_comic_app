d3f5688910b75e2b68aa2bec9d5ae89a9e83b9d9
FabianVarela <jfabian.303@gmail.com>
2023-01-18 18:03:50 -0500

    Replace removed widget by temp scaffold

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marvel_comic_app/app/app.dart';
-import 'package:marvel_comic_app/counter/counter.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(Scaffold), findsOneWidget);
     });
   });
 }
