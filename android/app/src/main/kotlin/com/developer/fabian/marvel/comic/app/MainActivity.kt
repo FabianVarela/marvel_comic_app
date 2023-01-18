bd16b0bcfb61bc0ebafec49e88ed1a3184baee6f
FabianVarela <jfabian.303@gmail.com>
2023-01-18 17:33:14 -0500

    Initialize project

---
+++ android/app/src/main/kotlin/com/developer/fabian/marvel/comic/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.developer.fabian.marvel.comic.app.marvel_comic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:marvel_comic_app/app/app.dart';
+import 'package:marvel_comic_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
