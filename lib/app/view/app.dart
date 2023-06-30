923c5e4fbeeb995580e84ca6895d3d67c5f5fff8
FabianVarela <jfabian.303@gmail.com>
2023-06-30 00:05:44 -0500

    Add route in app.dart

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:marvel_comic_app/features/comic_detail/view/comic_detail_view.dart';
+import 'package:marvel_comic_app/features/comic_list/view/comic_list_view.dart';
 import 'package:marvel_comic_app/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -15,7 +17,27 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const Scaffold(),
+      // You can use onGeneratedRoute pass arguments on settings param.
+      onGenerateRoute: (settings) {
+        if (settings.name == '/detail') {
+          final args = settings.arguments as String?;
+          return MaterialPageRoute(
+            builder: (_) => ComicDetailView(characterId: args!),
+          );
+        }
+        return null;
+      },
+      // You can use onGeneratedRoute pass arguments on settings param.
+      // The default or conventional way.
+      home: const ComicListView(),
+      /*
+      // Other option is use routes table defining key, widget and
+      //    get value with ModalRoute.of(context)!.settings.
+      routes: {
+        '/': (_) => const ComicListView(),
+        '/detail': (_) => const ComicDetailView(characterId: ''),
+      },
+      */
     );
   }
 }
